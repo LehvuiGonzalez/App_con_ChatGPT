6c73d065ceab62e75e8c4998f1e47534ace95199
LehvuiGonzalez <lehgonzalezca@unal.edu.co>
2024-11-18 16:06:28 -0500

    Update mi_segunda_app.py

--- mi_segunda_app.py
+++ mi_segunda_app.py
@@ -67,7 +67,7 @@ def convert_units(category, conversion_type, value):
     return conversions[conversion_type](value)
 
 # Configuración de la aplicación
-st.title("Conversor Universal en Tiempo Real")
+st.title("Conversor Universal")
 st.write("Selecciona una categoría, un tipo de conversión y proporciona un valor.")
 
 # Selección de categoría
